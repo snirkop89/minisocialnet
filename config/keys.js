ea8b768922d68b50a04bfe4d6bc292e5ed69af91
snirkop <snirkwork@gmail.com>
2018-12-24 22:10:51 +0200

    Inital express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: 'mongodb://snir:1234qwer!@ds123844.mlab.com:23844/socialdevnet'
+}
---
+++ routes/api/post.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+//@route GET api/posts/test
+//@desc Tests post route
+//@access Public
+
+router.get('/test', (req, res) => {
+  return res.json({ msg: 'Post works' })
+})
+
+module.exports = router
