49617cc3ef9c647b0eb06b24d9ccb578def6d48b
snirkop <snirkwork@gmail.com>
2018-12-24 23:52:12 +0200

    user registration route

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
 
 const users = require('./routes/api/users')
 const profile = require('./routes/api/profile')
 const post = require('./routes/api/post')
 
 const app = express()
+
+// Body parser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 // DB config
 const db = require('./config/keys').mongoURI
