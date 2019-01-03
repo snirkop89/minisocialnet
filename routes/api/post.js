38defc98579930d3debc444229af6a8340778998
Snir Koppelman <snirkwork@gmail.com>
2019-01-03 23:05:59 +0200

    add post like, unlike, add comment, remove comment

--- routes/api/post.js
+++ routes/api/post.js
@@ -87,4 +87,158 @@ router.delete(
   }
 )
 
+//@route POST api/posts/like/:id
+//@desc Like a post
+//@access Private
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id).then(post => {
+        if (
+          post.likes.filter(like => like.user.toString() === req.user.id)
+            .length > 0
+        ) {
+          return res
+            .status(400)
+            .json({ alreadyliked: 'User already liked this post' })
+        }
+
+        // Add user id to likes array
+        post.likes.unshift({ user: req.user.id })
+
+        post.save().then(post => res.json(post))
+      })
+    })
+  }
+)
+
+//@route POST api/posts/like/:id
+//@desc Like a post
+//@access Private
+router.post(
+  '/like/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id).then(post => {
+        if (
+          post.likes.filter(like => like.user.toString() === req.user.id)
+            .length > 0
+        ) {
+          return res
+            .status(400)
+            .json({ alreadyliked: 'User already liked this post' })
+        }
+
+        // Add user id to likes array
+        post.likes.unshift({ user: req.user.id })
+
+        post.save().then(post => res.json(post))
+      })
+    })
+  }
+)
+
+//@route POST api/posts/unlike/:id
+//@desc Unlike a post
+//@access Private
+router.post(
+  '/unlike/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id }).then(profile => {
+      Post.findById(req.params.id).then(post => {
+        if (
+          post.likes.filter(like => like.user.toString() === req.user.id)
+            .length === 0
+        ) {
+          return res
+            .status(400)
+            .json({ notliked: 'You have not yet liked this post' })
+        }
+
+        // Get the remove index
+        const removeIndex = post.likes
+          .map(item => item.user.toString())
+          .indexOf(req.user.id)
+
+        // Splice out of array
+        post.likes.splice(removeIndex, 1)
+
+        post.save().then(post => res.json(post))
+      })
+    })
+  }
+)
+
+//@route POST api/posts/comment/:id
+//@desc Comment on a post
+//@access Private
+router.post(
+  '/comment/:id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body)
+
+    //Check validation
+    if (!isValid) {
+      // if any errors send 400 with errors object
+      return res.status(400).json(errors)
+    }
+
+    Post.findById(req.params.id).then(post => {
+      const newComment = {
+        text: req.body.text,
+        name: req.body.name,
+        avatar: req.body.avatar,
+        user: req.user.id
+      }
+
+      // Add to comments array
+      post.comments.unshift(newComment)
+
+      post
+        .save()
+        .then(post => res.json(post))
+        .catch(err => res.status(404).json({ postnotfound: 'No post found' }))
+    })
+  }
+)
+
+//@route DELETE api/posts/comment/:id/:comment_id
+//@desc DELETE a comment on a post
+//@access Private
+router.delete(
+  '/comment/:id/:comment_id',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    Post.findById(req.params.id).then(post => {
+      // Check to see if comment exits
+      if (
+        post.comments.filter(
+          comment => comment._id.toString() === req.params.comment_id
+        ).length === 0
+      ) {
+        return res
+          .status(404)
+          .json({ commentnotexist: 'Comment does not exists' })
+      }
+
+      // Get remove index
+      const removeIndex = post.comments
+        .map(item => item._id.toString())
+        .indexOf(req.params.comment_id)
+
+      post.comments.splice(removeIndex, 1)
+
+      post
+        .save()
+        .then(post => res.json(post))
+        .catch(err => res.json(err))
+    })
+  }
+)
+
 module.exports = router
