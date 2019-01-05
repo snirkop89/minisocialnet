669267c89af9a08e96e1c47275732297cb61c92b
snirkop <snirkwork@gmail.com>
2019-01-06 00:12:34 +0200

    add login page and register page

--- client/src/components/auth/login.js
+++ client/src/components/auth/login.js
@@ -1,8 +1,66 @@
 import React, { Component } from 'react'
 
 class Login extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      errors: {}
+    }
+  }
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  onSubmit = e => {
+    e.preventDefault()
+    const user = {
+      email: this.state.email,
+      password: this.state.password
+    }
+    console.log(user)
+  }
+
   render() {
-    return <h1>Login</h1>
+    return (
+      <div class="login">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-8 m-auto">
+              <h1 class="display-4 text-center">Log In</h1>
+              <p class="lead text-center">
+                Sign in to your DevConnector account
+              </p>
+              <form onSubmit={this.onSubmit}>
+                <div class="form-group">
+                  <input
+                    type="email"
+                    class="form-control form-control-lg"
+                    placeholder="Email Address"
+                    name="email"
+                    value={this.state.name}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <div class="form-group">
+                  <input
+                    type="password"
+                    class="form-control form-control-lg"
+                    placeholder="Password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.onChange}
+                  />
+                </div>
+                <input type="submit" class="btn btn-info btn-block mt-4" />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
   }
 }
 
